930c0ddc147ef8d63c22f8a2337e2b3c9f60a392
Griesser, Nadege <Nadege.Griesser@vector.com>
2022-11-20 10:28:22 +0100

    set args externally

--- code-ranking-updater.js
+++ code-ranking-updater.js
@@ -2,22 +2,18 @@ const { By, Builder } = require("selenium-webdriver");
 require('chromedriver');
 const chrome = require('selenium-webdriver/chrome');
 
-async function updateAllCodesRanking() {
+async function updateAllCodesRanking(args) {
   let driver = await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(new chrome.Options().headless())
     .build();
   try {
-    await driver.get('https://www.topparrain.com/en/users/sign_in');
+    await driver.get(args[0] + '/users/sign_in');
     //await driver.findElement(By.xpath("//button[@aria-label='Consent']")).click();
-    await driver.findElement(By.id('user_email')).sendKeys('ngmg.test@gmail.com');
-    let field = await driver.findElement(By.id('user_password'));
-    for (let i = 0; i < 8; i++) {
-      await field.sendKeys('TP');
-    }
-    await field.sendKeys('8!');
+    await driver.findElement(By.id('user_email')).sendKeys(args[1]);
+    await driver.findElement(By.id('user_password')).sendKeys(args[2]);
     await driver.findElement(By.xpath("//input[@value='Log in']")).click();
-    await driver.get('https://www.topparrain.com/en/referral_codes');
+    await driver.get(args[0] + '/referral_codes');
     await driver.findElement(By.id('update_all_codes_ranking')).click();
     console.log(await driver.findElement(By.id('update_ranking')).findElement(By.xpath('./div[2]/div')).getText());
   } catch (ex) {
@@ -29,5 +25,6 @@ async function updateAllCodesRanking() {
   }
 }
 
-updateAllCodesRanking();
+let args = process.argv.slice(2);
+updateAllCodesRanking(args);
 
